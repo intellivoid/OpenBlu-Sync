6c7c416f695e2b1845ef8cdc2cf9dd3cd4d3280e
Netkas <45276434+Netkas@users.noreply.github.com>
2019-08-19 00:57:58 -0400

    UwU

--- src/index.php
+++ src/index.php
@@ -5,3 +5,4 @@
     print(hash('sha256', time() . "T^T") . "<br/>");
     print(hash('sha256', time() . "O.O") . "<br/>");
     print(hash('sha256', time() . "*-*") . "<br/>");
+    print(hash('sha256', time() . "0W0") . "<br/>");
