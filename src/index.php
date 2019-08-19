1394e493bb7fad6317f56dac28585259145d6478
Netkas <45276434+Netkas@users.noreply.github.com>
2019-08-19 00:57:08 -0400

    OwO

--- src/index.php
+++ src/index.php
@@ -1,3 +1,7 @@
 <?php
 
-    print(hash('sha256', time() . "OwO"));
+    print(hash('sha256', time() . "OwO") . "<br/>");
+    print(hash('sha256', time() . "UwU") . "<br/>");
+    print(hash('sha256', time() . "T^T") . "<br/>");
+    print(hash('sha256', time() . "O.O") . "<br/>");
+    print(hash('sha256', time() . "*-*") . "<br/>");
